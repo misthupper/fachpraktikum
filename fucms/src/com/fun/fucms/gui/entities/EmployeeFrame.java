203c28c24888a08546a09ab61afb8697c5459e96
hrod99 <hrod99@222b415a-ad3e-0410-8f95-45b98ee436a5>
2007-12-30 13:21:19 +0000

    added EntytyPanel

--- fucms/src/com/fun/fucms/gui/entities/EmployeeFrame.java
+++ fucms/src/com/fun/fucms/gui/entities/EmployeeFrame.java
@@ -23,26 +23,17 @@ public class EmployeeFrame extends JFrame {
 	private static final String SELECTION_TEXT = "Auswahl";
 	
 	private static final String ENTITY_TEXT = "Mitarbeiter";
-	private static final String ID_LABEL_TEXT = "ID: ";
-	private static final String SURNAME_LABEL_TEXT = "Name: ";
-	private static final String FIRSTNAME_LABEL_TEXT = "Vorname: ";
-	private static final String BUTTON_OK_TEXT = "OK";
-	private static final String BUTTON_CANEL_TEXT = "Cancel";
 	
 	private Container mContainer;
 	private JTabbedPane mJTabbedPane;
-	private JPanel mSelectPanel, mEntityPanel;
+	private JPanel mSelectPanel; 
+	private EntityPanel mEntityPanel;
 	
-	private JLabel mIdLabel, mSNameLabel, mFNameLabel;
-	private JTextField mIdField, mSNameField, mFNameField;
-	private Box mEntityBox, mIdBox, mSNameBox, mFNameBox, mButtonBox;
-	private JButton mSaveButton, mCancelButton;
 	private JScrollPane mTableScrollPane;
 	
 	private JTable mJTable;
 	private EntityTableModel mEntityTableModel;
 	
-	private EmployeeController mEmployeeController;
 	
 	
 	public EmployeeFrame() {
@@ -60,53 +51,12 @@ public class EmployeeFrame extends JFrame {
         mJTabbedPane = new JTabbedPane();
         mSelectPanel = new JPanel();
         mSelectPanel.setLayout(new BorderLayout());
-        mEntityPanel = new JPanel();
-        mEntityPanel.setLayout(new BorderLayout());
-        mEntityBox = new Box(BoxLayout.PAGE_AXIS);
+        mEntityPanel = new EntityPanel(new Person());
         mJTabbedPane.add(SELECTION_TEXT,mSelectPanel);
         mJTabbedPane.add(ENTITY_TEXT,mEntityPanel);
-        
-        mEmployeeController = new EmployeeController(this);
-        
-        //EntityPanel
-        
-        mIdLabel = new JLabel(ID_LABEL_TEXT);
-        mIdField = new JTextField();
-        mIdBox = new Box(BoxLayout.LINE_AXIS);
-        mIdBox.add(mIdLabel);
-        mIdBox.add(mIdField);
-        
-        mSNameLabel = new JLabel(SURNAME_LABEL_TEXT);
-        mSNameField = new JTextField();
-        mSNameBox = new Box(BoxLayout.LINE_AXIS);
-        mSNameBox.add(mSNameLabel);
-        mSNameBox.add(mSNameField);
-        
-        mFNameLabel = new JLabel(FIRSTNAME_LABEL_TEXT);
-        mFNameField = new JTextField();
-        mFNameBox = new Box(BoxLayout.LINE_AXIS);
-        mFNameBox.add(mFNameLabel);
-        mFNameBox.add(mFNameField);
-        
-        mSaveButton = new JButton(BUTTON_OK_TEXT);
-        mSaveButton.addActionListener(mEmployeeController);
-        mSaveButton.setActionCommand(EmployeeController.ACTION_OK);
-        mCancelButton = new JButton(BUTTON_CANEL_TEXT);
-        mCancelButton.addActionListener(mEmployeeController);
-        mCancelButton.setActionCommand(EmployeeController.ACTION_CANCEL);
-        mButtonBox = new Box(BoxLayout.LINE_AXIS);
-        mButtonBox.add(mCancelButton);
-        mButtonBox.add(Box.createHorizontalGlue());
-        mButtonBox.add(mSaveButton);
 
-        mEntityBox.add(mIdBox);
-        mEntityBox.add(mSNameBox);
-        mEntityBox.add(mFNameBox);
-        
-        mEntityPanel.add(mEntityBox,BorderLayout.NORTH);
-        mEntityPanel.add(mButtonBox,BorderLayout.SOUTH);
-        
         mContainer.add(mJTabbedPane,BorderLayout.CENTER);
+
         
         //SelectPanel
         
---
+++ fucms/src/com/fun/fucms/gui/entities/EntityPanel.java
@@ -0,0 +1,65 @@
+package com.fun.fucms.gui.entities;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.fun.fucms.controller.EmployeeController;
+import com.fun.fucms.model.Entity;
+
+public class EntityPanel extends JPanel implements ActionListener {
+	
+	private static final String BUTTON_OK_TEXT = "OK";
+	private static final String BUTTON_CANEL_TEXT = "Cancel";
+	
+	private Box mFieldBox, mButtonBox;
+	private ArrayList<JTextField> mTextFields = new ArrayList<JTextField>();
+	private JButton mSaveButton,mCancelButton;
+	
+	public EntityPanel(Entity e) {
+		super();
+		this.setLayout(new BorderLayout());
+		mFieldBox = new Box(BoxLayout.PAGE_AXIS);
+		mButtonBox = new Box(BoxLayout.LINE_AXIS);
+		this.add(mFieldBox,BorderLayout.NORTH);
+		this.add(mButtonBox,BorderLayout.SOUTH);
+		
+		for (int i = 0 ; i < e.getFields().length ; i++) {
+			Box box = new Box(BoxLayout.LINE_AXIS);
+			mFieldBox.add(box);
+			box.add(new JLabel(e.getFields()[i] + " : "));
+			JTextField jtextField = new JTextField();
+			jtextField.setName(e.getFields()[i]);
+			jtextField.setText(e.getValueAsString(i));
+			box.add(jtextField);
+			mTextFields.add(jtextField);
+		}
+	    
+	    mSaveButton = new JButton(BUTTON_OK_TEXT);
+	    mSaveButton.addActionListener(this);
+	    mSaveButton.setActionCommand(EmployeeController.ACTION_OK);
+	    mCancelButton = new JButton(BUTTON_CANEL_TEXT);
+	    mCancelButton.addActionListener(this);
+	    mCancelButton.setActionCommand(EmployeeController.ACTION_CANCEL);
+	    mButtonBox.add(mCancelButton);
+	    mButtonBox.add(Box.createHorizontalGlue());
+	    mButtonBox.add(mSaveButton);
+	
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+}
